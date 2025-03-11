ed11daf8dcf97b4f3537e2bc6770bad24bae7270
AnshulDogra001 <abhinav.dogra001@gmail.com>
2025-03-12 00:51:10 +0530

    final

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ app.include_router(email.router, prefix="/api")
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to the Email Processing API"}
+    return {"message": "Welcome to the Email Processing API Updated"}
